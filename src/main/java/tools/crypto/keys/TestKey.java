68c815cd32b0362ff06b1091fa4fdbb5563a3533
sgordon <sgordon@ubuntu-mobule>
2014-10-16 19:45:52 -0400

    Updating the testkey to use bouncyCastle

--- src/main/java/tools/crypto/keys/TestKey.java
+++ src/main/java/tools/crypto/keys/TestKey.java
@@ -1,12 +1,11 @@
 package tools.crypto.keys;
 
-import java.lang.reflect.UndeclaredThrowableException;
-import java.security.GeneralSecurityException;
+import org.bouncycastle.crypto.digests.SHA1Digest;
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.util.encoders.Hex;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
-import tools.crypto.OtpHashMode;
 
 public class TestKey implements IKeyProvider {
 	private final byte[] KEY;
@@ -15,16 +14,26 @@ public class TestKey implements IKeyProvider {
 	}
 
 	//HMAC computes a Hashed Message Authentication Code with the crypto hash algorithm as a parameter.
-    public byte[] ComputeHmac(String mode, byte[] text){
-        try {
-            Mac hmac;
-            hmac = Mac.getInstance(mode);
-            SecretKeySpec macKey =
-                new SecretKeySpec(KEY, "RAW");
-            hmac.init(macKey);
-            return hmac.doFinal(text);
-        } catch (GeneralSecurityException gse) {
-            throw new UndeclaredThrowableException(gse);
-        }
+    public byte[] ComputeHmac(String mode, byte[] text) {
+        HMac hmac;
+		if(mode.equalsIgnoreCase("sha1")){
+			SHA1Digest digest =  new SHA1Digest();
+			hmac=new HMac(digest);
+		}
+		else{
+			SHA256Digest digest = new SHA256Digest();
+			hmac=new HMac(digest);
+		}
+		byte[] resBuf=new byte[hmac.getMacSize()];
+		hmac.init(new KeyParameter(KEY));
+		hmac.update(text,0,text.length);
+		hmac.doFinal(resBuf,0);
+		return resBuf;
     }
+    
+
+	@Override
+	public String getHexString(byte[] bytes) {
+		return Hex.encode(bytes).toString();
+	}
 }
